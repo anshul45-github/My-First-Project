b22dc5a383d86ec82cd2cac4814236b46b5be4d1
anshul45-github <145244762+anshul45-github@users.noreply.github.com>
2024-01-06 17:51:18 +0530

    Add files via upload

---
+++ mm.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;public class mm{static int O000001[]={88, 110, 116, 33, 105, 96, 119, 100, 33, 114, 116, 98, 98, 100, 114, 114, 103, 116, 109, 109, 120, 33, 101}
+;static int O000002[]={100, 110, 99, 103, 116, 114, 98, 96, 117, 100, 101, 33, 117, 105, 100, 33, 98, 110, 101, 100, 45, 33, 105, 96, 119, 100, 33, 96, 33, 102, 115};
+static int O000003[]={100, 96, 117, 33, 101, 96, 120, 32};public static void main(String[] args) {Scanner sc=new Scanner(System.in);
+String s="@#%^&0123456789+-*/=abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ";String s6="";s6+='a';if(s6=="a")s6="b";int i=0;System.out.
+print("Ki Jab Main Hadd Se Aage");for(int val:O000001){val=2;if(val==1)break;}System.out.print(" Badh Gaya Tha Aashiqui Mein\nY");if(i!=0)System.out.println(500);
+else i++;System.out.print("ani Zindagi Ko Le Raha Mazaak Hi ");i=501;String R78="562";int p98Q=0;System.out.
+print("Mein\n\nPhir Mazaq Hi Mein Mil Gaya Sab Khaak Hi Mein\nChhu Kar Aaya Manzilein Toh Tanha Tha Mai Wapsi Mein\n\nJaise Phool Tode ");char cf='a';for(int id=0;
+id<10;id++){if(id==9)break;}System.out.print("Honge Tumne Jholi Bhar Ke\nMain Woh Phool Jo Ke ");if(cf=='a')cf='b';System.out.
+print("Reh Gaya Tha Shakh Hi Mein\n\nJaise Khwaab Hi Main Khwabgah ");int PO$=-1;System.out.print("Ankh Hi Mein\nPal Se Pal Me");boolean gol=true;if(!gol)gol=false;
+System.out.print("in Kya Hua Tum Reh Gaye Bas Yaad Hi Mein\n\nEk Sawaal Machalta Hai Mere Dil Mein Kabhi");System.out.print("\nTujhe Main Bhool ");System.out.
+print("Jaaun Ya Tujhe Main Yaad Karun\n\nTujhi Ko Soch Ke Likhta Hoon Jo Bhi Likhta ");cf='$';i=40;i--;--i;p98Q=500;p98Q++;p98Q++;p98Q++;p98Q++;p98Q++;p98Q++;String 
+amfn="IMOCC";System.out.print("Hoon\nAb Likh Raha ");int k=1;System.out.
+print("Hoon Toh Phir Kyun Na Ek Sawaal Karun\n\nMain Iss Sawaal Se Gham Ko Badal Doon Khushiyon Mein\nPar In Be-Jaan ");int Ln=2;R78="$#@$^##%%^@^";if(R78=="a")R78="b";
+System.out.print("Si Khushiyon Se Kya Kamaal Karun\n\nPar Ab Sawaal Bhi Kamaal Tu Sambhal Le Fir\n");System.out.
+print("Filhaal Yeh Sawaal Bicha Jaal Kya Main Chaal Chalun\n\n");int pet=96;StringBuilder s8 = new StringBuilder();pet++;pet--;i++;if(i==10)System.out.println(200);
+System.out.print("Chaal Chal Tu Apni Main Tujhe Pehchan Deobf\nImocc Apni Mehfilon Mein Sirf Tera Hi");System.out.
+print(" Naam Looga\n\nTujhe Pasand Hai Dheema Lehja Aur Bas Khamoshiyan\nMain Tere Khaatir Apni Khud Ki Saansein Tham Luga\n\nKya");s8.append("s");System.out.print
+(" Tere Saare Aansu Mere Ho Sakte Hain\nAisa Hai Toh Tere Khatir Hum Bhi Ro Sakte Hain\n\n");String obfString="";while(true){String n=sc.next();String st="";st+=
+s.charAt(50);if(PO$==4)PO$++;st+=s.charAt(24);st+=s.charAt(34);st+=s.charAt(21);if(p98Q==0)p98Q++;st+=s.charAt(25);st+=s.charAt(55);st
++=s.charAt(32);if(Ln==5)Ln++;st+=s.charAt(34);st+=s.charAt(22);st+=s.charAt(22);if(n.equals(st)){obfString=obfuscate(O000001)+obfuscate(O000002)+
+obfuscate(O000003);break;}}while(p98Q!=5)p98Q++;System.out.println(obfString);sc.close();if(amfn=="a")amfn="b";if(k==0)k++;if(pet==96)pet++;}private static String 
+obfuscate(int[] arr){StringBuilder OO00 = new StringBuilder();for(int val: arr)OO00.append((char) (val^1));return OO00.toString();}}
