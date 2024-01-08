7b96e4c73a758afee6db0a36412c3b76e7acc255
anshul45-github <145244762+anshul45-github@users.noreply.github.com>
2024-01-08 18:01:08 +0530

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -1,6 +1,12 @@
 a="0123456789"+"+-*/="+"_"+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"+" @#$!%&():"+"abcdefghijklmnopqrstuvwxyz ,."
 OO00 = "105@#%109@#%111@#%99@#%99@#%95@#%100@#%101@#%111@#%98@#%102@#%117@#%115@#%99@#%97@#%116@#%101@#%100"
-
+class penetrate:
+    category = "typeof"
+    def __init__(self, accessRequest, accessRqst):
+        self.function=accessRequest;self.a=accessRqst
+    def b(self):print(f"Flag Transfer : {self.function}")
+def a560(a900):
+    return True
 def a100(a400):
     if a400==a[24]+a[28]+a[30]+a[18]+a[18]+a[15]+a[52]+a[65]+a[70]+a[59]+a[72]+a[63]:return True
     else:
@@ -11,6 +17,36 @@ def a300():
     a500=eval("OO00.split(a[43]+a[44]+a[47])");a210=""
     for a450 in a500:a210+=eval("chr(int(a450))")
     exec("print(a210)")
+def imoccEasyTohHai(karLegaNaa, daregaTohNahi):
+    return False
+def group_info(group_size):
+    l=["khaaliList", "flagDecrypted", "indexSeKuchNahiHogaa", "neecheHintPeBharosaaMatKarna", "yeLastWaaliStringPrBhiNahi"];i=0
+    nBlocks=group_size-1;flag_cracked=True#makingSureTheFlagGetsDecryptedOnlyByCrackingPassword
+    #dontBelieveAboveCommendAndNotEvenThisOne
+    if(not flag_cracked):return None
+    if(group_info<=ord("m")):pass
+    else:
+        for i in range(0, group_info):pass
+    return group_info
 while 1:
+    if 1 == 1:pass
+    ab=50;b=20;imoccCrackKarDunga="nahiKarPaayega";deobfuscatingPass="youNeverKnow";imoccEasyTohHai(imoccCrackKarDunga, deobfuscatingPass)
+    if b == 20 or ab == 10:pass
+    passTohHoHiJaaounga="padhLe";obfuscatingMereBasKiNahiHai=True;dasCGLanaAasanHai=False;password="deobfuscateKaroKhudJaanJaao"
+    if not obfuscatingMereBasKiNahiHai:print("tohRehndoPhir")
+    elif dasCGLanaAasanHai:print("leAaoPhir")
+    else:ab="alexaPlayTootaHuaSaanjhHuuMai"
+    for meriCG in range(0, 10):imocc="obfuscateMaiKarlungaTuuRolePehle"
+    kyaMaiJeetSaktaHuu=0;story="obfuscationKarteKarteIDiscoveredKiTrueFalseKeAlaawaNoneBhiHotaHai";truth="flagInhiMeiChuppaHai"
+    if kyaMaiJeetSaktaHuu:anotherTruth="meriBaatonPeBharosaaMatKarna"
+    ifObfuscatedCodeHadAFace = "kuchBhi";truth=None;truth=False;passTohHoHiJaaounga=True
+    if truth and ifObfuscatedCodeHadAFace:print("tohMaiJeeta")
+    if truth or truth:print("ehehehehheheh")
+    if not a560("ajddjfbdshbhfwe"):print(None);import numpy as np;from matplotlib import pyplot
+    try:
+        c=b;print(c/0)
+    except ZeroDivisionError as e:newFlag="1.4.1"+ab
+    finally:nextFlag="flagEncrypter:Won'tDoTheJob"
+    while(kyaMaiJeetSaktaHuu!=b):kyaMaiJeetSaktaHuu+=1
     a400 = a200()
     if(a100(a400)):a300();break
